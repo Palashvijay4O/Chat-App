64069a79174eed2e87fbd5eaf2a93009b46ebce6
Palash Vijay <palashvijay13319@gmail.com>
2020-10-01 13:07:05 +0530

    new loading image

--- public/js/index.js
+++ public/js/index.js
@@ -9,7 +9,6 @@ ReactDOM.render(
     document.getElementById('root')
 )
 
-
 document.querySelector('#loginForm').addEventListener('submit', (event) => {
     localStorage.setItem('username', document.querySelector('#loginForm').querySelector('input[name="username"]').value)
     localStorage.setItem('room', document.querySelector('#loginForm').querySelector('input[name="room"]').value)
